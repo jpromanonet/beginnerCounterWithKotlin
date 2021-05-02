e72fd883a9301fd9fab44e7ddd1a94f4d9c5def1
Juan P. Romano <programming@jpromano.net>
2021-05-01 23:00:15 -0300

    Text view modification

--- app/src/main/java/com/example/beginnercounter/MainActivity.kt
+++ app/src/main/java/com/example/beginnercounter/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val initialTextViewTranslationY = textView_progress.translationY
+
+        seekBar.setOnSeekBarChangeListener()
     }
 }
